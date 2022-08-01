261ff6b6939bef42191d1955ef9da82b11065a01
Manuch Sadri <colonquebang@hotmail.com>
2022-07-31 23:57:06 -0700

    resolved vs code & eslint warnings

--- js/app.js
+++ js/app.js
@@ -29,37 +29,14 @@ function runSalesReport(){
     printReportHeader();
     // calculate the sales data and print the sales report body
     for (let i = 0; i < stores.length; i++){
-
+        // calculate cookies sales for each store location
         calcSales(i);
 
         //renderReport(i); // call render method to populate store data in for loop
 
     }
-
     // function call print column footer
-
-}
-
-// calculate hourly sales data and insert data into sales[] array
-function calcSales(i){
-    let salesTotal = 0; // holds iterating sum of the day's hourly sales
-    // hrsOpen.length - 1 to account for the added hrsOpen[] element to hold Total
-    for (let j = 0; j < hrsOpen.length - 1; j++){
-        // repeat the getSales() method from the store location oject to calculate sales data for each hour the store is open
-        stores[i].sales.push(stores[i].getSales());
-        // add store's hourly sales to the store's daily sales total
-        salesTotal += stores[i].sales[j];
-        // add the store's hourly sales to the hourly grand total 
-        grandTotals[j] += stores[i].sales[j];
-        // if end of day add the days's sales total to the end of the sales data array
-        // hrsOpen.length - 2 to account for the added hrsOpen[] element to hold Total
-        if (j === hrsOpen.length - 2){
-            // add the total sales to the end of sales[]
-            stores[i].sales.push(salesTotal);
-            // calculate the daily grand total sales by adding each of the store's daily totals
-            grandTotals[j+1] += salesTotal;
-        }
-    }
+    printReportFooter();
 }
 
 // function to print the sales report header row using hours of operation in hrsOpen[]
@@ -67,7 +44,7 @@ function printReportHeader(){
     // select the 'table-header' html id container element
     let tableHeader = document.getElementById('table-header');
     // create new table row for header
-    let headerRow = document.createElement('tr');
+    let headerRow = document.createElement('tr'); // eslint-disable-line
     // create blank table header cell
     let blank = document.createElement('th');
     blank.innerText = null;
@@ -80,14 +57,45 @@ function printReportHeader(){
     }
 }
 
-
+// calculate hourly sales data and insert data into sales[] array
+function calcSales(i){
+    let salesTotal = 0; // holds iterating sum of the day's hourly sales
+    // hrsOpen.length - 1 to account for the added hrsOpen[] element to hold Total
+    for (let j = 0; j < hrsOpen.length - 1; j++){
+        // repeat the getSales() method from the store location oject to calculate sales data for each hour the store is open
+        stores[i].sales.push(stores[i].getSales());
+        // add store's hourly sales to the store's daily sales total
+        salesTotal += stores[i].sales[j];
+        // add the store's hourly sales to the hourly grand total
+        grandTotals[j] += stores[i].sales[j];
+        // if end of day add the days's sales total to the end of the sales data array
+        // hrsOpen.length - 2 to account for the added hrsOpen[] element to hold Total
+        if (j === hrsOpen.length - 2){
+            // add the total sales to the end of sales[]
+            stores[i].sales.push(salesTotal);
+            // calculate the daily grand total sales by adding each of the store's daily totals
+            grandTotals[j+1] += salesTotal;
+        }
+    }
+}
 
 // function to print the sales report's footer row
 function printReportFooter(){
-
-
+    // select the 'table-footer' html id container element
+    let tableFooter = document.getElementById('table-footer');
+    // create new table row for sales totals footer
+    let footerRow = document.createElement('tr'); // eslint-disable-line
+    // create Totals table footer(<th>) cell
+    let totals = document.createElement('th');
+    totals.innerText = 'Totals';
+    tableFooter.appendChild(totals);
+    // fill-in the totals footer row with hourly and daily sales totals from grandTotals()
+    for (let j = 0; j < grandTotals.length; j++){
+        let colTotal = document.createElement('th');
+        colTotal.innerText = grandTotals[j];
+        tableFooter.appendChild(colTotal);
+    }
 }
-
 
 // create store objects using the Store() constructor, and add objects to array stores[]
 let seattle = new Store('Seattle',	23,	65,	6.3);
